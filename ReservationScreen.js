5686d9cef72b1c2718c671758b151e085fbb2643
jahredperry <130723557+jahredperry@users.noreply.github.com>
2023-07-27 18:40:26 -0400

    Update ReservationScreen.js

--- ReservationScreen.js
+++ ReservationScreen.js
@@ -45,26 +45,23 @@ const ReservationScreen = () => {
                 {
                     text: 'OK',
                     onPress: () =>
-
-                        
-                        
+             ],
+            { cancelable: false }
+        );             
                 }
             ]
         );
-    {
         console.log('campers:', campers);
         console.log('hikeIn:', hikeIn);
         console.log('date:', date);
-
-    };
 };
+    
 const resetForm = () => {
     setCampers(1);
     setHikeIn(false);
     setDate(new Date());
     setShowCalendar(false);
-
-
+    
     return (
         <ScrollView>
             <Animatable.View
@@ -122,35 +119,10 @@ const resetForm = () => {
                         accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
-                <View style={styles.modal}>
-                    <Text style={styles.modalTitle}>
-                        Search Campsite Reservations
-                    </Text>
-                    <Text style={styles.modalText}>
-                        Number of Campers: {campers}
-                    </Text>
-                    <Text style={styles.modalText}>
-                        Hike-In?: {hikeIn ? 'Yes' : 'No'}
-                    </Text>
-                    <Text style={styles.modalText}>
-                        Date: {date.toLocaleDateString('en-US')}
-                    </Text>
-                    <Button
-                        onPress={() => {
-                            setShowModal(!showModal);
-                            resetForm();
-                        }}
-                        color='#5637DD'
-                        title='Close'
-                    />
-                </View>
             </Animatable.View>
         </ScrollView>
     );
 };
-
-
-
 
 const styles = StyleSheet.create({
     formRow: {
@@ -167,22 +139,7 @@ const styles = StyleSheet.create({
     formItem: {
         flex: 1
     },
-    modal: {
-        justifyContent: 'center',
-        margin: 20
-    },
-    modalTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        backgroundColor: '#5637DD',
-        textAlign: 'center',
-        color: '#fff',
-        marginBottom: 20
-    },
-    modalText: {
-        fontSize: 18,
-        margin: 10
-    }
+    
 });
 
 export default ReservationScreen;
